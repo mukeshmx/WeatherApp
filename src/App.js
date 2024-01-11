05d9c9b1fb2de5b6ab7423989241c189154d9199
Mukesh M <xmukeshm@gmail.com>
2024-01-11 21:31:22 +0530

    Updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherApp from './component/weatherApp/weatherapp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
